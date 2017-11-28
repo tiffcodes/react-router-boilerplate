adedb2f7ddf906a3cbdf67c8a2d6612959842541
Tiff Nogueira <tiffnogueira@gmail.com>
2017-11-28 11:39:10 -0500

    updated gulp file and dependencies

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+class Catalogue extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      <div>
+        cat!
+      </div>
+    )
+  }
+}
+
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          <header className='top-header'>
+            <h1>HackFlix</h1>
+            <nav>
+              <a href="#">Catalogue</a>
+            </nav>
+          </header>
+          <Catalogue />
         </div>
       )
     }
